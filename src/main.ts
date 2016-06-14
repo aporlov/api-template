b84f92c22827af183ffde1e98d85191d92fdcfb4
Orlov Alexey <aporlov@gmail.com>
2016-06-14 12:17:36 +0300

    Refactoring

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import {config} from 'dotenv';
-import * as express from  'express'  ;
+import * as express from  'express';
 import * as  mongoose from 'mongoose';
+import * as morgan form 'morgan';
 config();
 const db = mongoose.createConnection('mongodb://localhost/prices');
 const app = express();
@@ -8,17 +9,17 @@ const port = process.env.PORT || 3000;
 
 
 app.get('/', (req,res)=>{
-    res.send('welcome to my API')
+    res.send('welcome to my API');
 });
 
-app.get('*', function(req, res){
+app.get('*', (req, res)=>{
   res.status(404).send();
 });
 
 
 
-app.listen(port,()=> {
-    console.log('Running on PORT:' + port)
+app.listen(port, ()=>{
+    console.log('Running on PORT:' + port);
 });
 
 exports.app = app;
---
+++ src/typings/index.d.ts
@@ -0,0 +1,1 @@
+/// <reference path="globals/morgan/index.d.ts" />
