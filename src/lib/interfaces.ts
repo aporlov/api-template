e967ce3fc8b6791aac14726a20dbdce1a7c027f6
Orlov Alexey <aporlov@gmail.com>
2016-06-22 11:04:38 +0300

    final stage

--- src/lib/interfaces.ts
+++ src/lib/interfaces.ts
@@ -4,5 +4,5 @@ My Interfaces
 // for hashing and comparing password 
 export interface iHash {
     create(password:string, callback:(err:any,password:string)=>void): void;
-    compare(password:string,hash:string, callback:(err:any,result:boolean)=>void ):void;
+    compare(password:string, hash:string, callback:(err:any,result:boolean)=>void):void;
 }
